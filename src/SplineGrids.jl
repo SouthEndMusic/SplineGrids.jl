9f7f6360e5e2a340cffedb484f7f8a9870506cc9
Bart de Koning <bart123-4@hotmail.com>
2024-11-30 11:41:18 +0100

    Compute basis functions with KA kernel

--- src/SplineGrids.jl
+++ src/SplineGrids.jl
@@ -1,25 +1,29 @@
 module SplineGrids
 
+using KernelAbstractions
 using RecipesBase
 
 include("knot_vector.jl")
 include("spline_dimension.jl")
 include("plot_rec.jl")
 
-struct SplineGrid{S <: SplineDimension, C <: AbstractArray, W <: Union{AbstractArray, Nothing}, E <: AbstractArray}
+struct SplineGrid{S <: SplineDimension, C <: AbstractArray,
+    W <: Union{AbstractArray, Nothing}, E <: AbstractArray}
     spline_dimensions::Vector{S}
     control_points::C
     weights::W
     eval::E
     function SplineGrid(spline_dimensions, control_points, weights, eval)
         # TODO: Add validation of combination of control points, weights, and basis functions
-        new{eltype(spline_dimensions), typeof(control_points), typeof(weights), typeof(eval)}
+        new{eltype(spline_dimensions), typeof(control_points),
+            typeof(weights), typeof(eval)}
     end
 end
 
 # TODO: Add indexing functionality such that indexing a spline_grid implies indexing spline_grid.eval
 
-function SplineGrid(spline_dimensions::Vector{<:SplineDimension}, control_points::AbstractVector)::SplineGrid
+function SplineGrid(spline_dimensions::Vector{<:SplineDimension},
+        control_points::AbstractVector)::SplineGrid
     # TODO: allocate eval array
     weights = nothing
     SplineGrid(spline_dimensions, control_points, weights, eval)
@@ -30,6 +34,6 @@ function evaluate!(spline_grid::SplineGrid)::Nothing
     return nothing
 end
 
-export KnotVector, SplineDimension, SplineGrid, decompress
+export KnotVector, SplineDimension, SplineGrid, evaluate!
 
 end # module SplineGrids
