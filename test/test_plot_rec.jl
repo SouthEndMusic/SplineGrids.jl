fe5bdcc56a8edba8f586358017f2109cffed91d0
Bart de Koning <bart123-4@hotmail.com>
2024-12-22 14:38:42 +0100

    Also test spline dimension plotting

--- test/test_plot_rec.jl
+++ test/test_plot_rec.jl
@@ -4,8 +4,26 @@ using Plots
 n_control_points = (5, 6)
 degree = (3, 2)
 n_sample_points = (32, 55)
+max_derivative_order = 1
 
-spline_dimensions = SplineDimension.(n_control_points, degree, n_sample_points)
+spline_dimensions = SplineDimension.(
+    n_control_points, degree, n_sample_points; max_derivative_order)
+
+@testset "Plot spline dimension" begin
+    @test try
+        plot(spline_dimensions[1])
+        true
+    catch
+        false
+    end
+
+    @test try
+        plot(spline_dimensions[1]; derivative_order = 1)
+        true
+    catch
+        false
+    end
+end
 
 dimensionalities = [
     ((1, Nout) for Nout in 1:4)...,
@@ -13,7 +31,7 @@ dimensionalities = [
 ]
 
 for (Nin, Nout) in dimensionalities
-    @testset "Nin = $Nin, Nout = $Nout" begin
+    @testset "Plot spline grid, Nin = $Nin, Nout = $Nout" begin
         spline_grid = SplineGrid(spline_dimensions[1:Nin], Nout)
         @test try
             plot(spline_grid)
