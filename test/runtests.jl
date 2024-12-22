555fd0d8e8ed0f1eccfe07872e6377b0d844d592
Bart de Koning <bart123-4@hotmail.com>
2024-12-22 13:39:56 +0100

    Add utility tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using SafeTestsets
 
+@safetestset "Utilities" include("test_utils.jl")
 @safetestset "KnotVector" include("test_knot_vector.jl")
 @safetestset "SplineDimension" include("test_spline_dimension.jl")
 @safetestset "SplineGrid" include("test_spline_grid.jl")
-@safetestset "LinearMaps" include("test_LinearMapsExt.jl")
-@safetestset "Enzyme" include("test_EnzymeExt.jl")
+@safetestset "LinearMapsExt" include("test_LinearMapsExt.jl")
+@safetestset "EnzymeExt" include("test_EnzymeExt.jl")
 @safetestset "Aqua" include("aqua.jl")
