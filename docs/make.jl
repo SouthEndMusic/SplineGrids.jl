8817f855cc81345dded6cd63c24e4fbec1774f04
Bart de Koning <bart123-4@hotmail.com>
2025-01-26 13:14:58 +0100

    Restore make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,22 +8,22 @@ makedocs(modules = [SplineGrids],
     pagesonly = true,
     linkcheck_ignore = ["https://www.ag.jku.at/pubs/2016gjkmss.pdf"],
     pages = [
-    # "index.md",
-    # "Theory" => [
-    #     "Refinement" => "theory_refinement.md",
-    #     "Local refinement (THB-splines)" => "theory_local_refinement.md"
-    # ],
-    # "Examples" => [
-    #     "Dimensionality" => "examples_dimensions.md",
-    #     "NURBS" => "examples_nurbs.md",
-    #     "Derivatives" => "examples_derivatives.md",
-    #     "Linear fitting" => "examples_linear_fitting.md",
-    #     "Control point derivatives with Enzyme" => "examples_enzyme.md"
-    # ],
-    # "Advanced examples" => [
-    #     "Solving a PDE" => "examples_pde.md",
-    #     "Optimizing a lens surface" => "examples_optics.md"
-    # ],
+        "index.md",
+        "Theory" => [
+            "Refinement" => "theory_refinement.md",
+            "Local refinement (THB-splines)" => "theory_local_refinement.md"
+        ],
+        "Examples" => [
+            "Dimensionality" => "examples_dimensions.md",
+            "NURBS" => "examples_nurbs.md",
+            "Derivatives" => "examples_derivatives.md",
+            "Linear fitting" => "examples_linear_fitting.md",
+            "Control point derivatives with Enzyme" => "examples_enzyme.md"
+        ],
+        "Advanced examples" => [
+            "Solving a PDE" => "examples_pde.md",
+            "Optimizing a lens surface" => "examples_optics.md"
+        ],
         "Manual" => "manual.md"
     ]
 )
